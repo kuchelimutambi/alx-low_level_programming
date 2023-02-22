b2f042ed9b1aa5cc30bdf27b15261ac5085ad09c
kuchelimutambi <kuchelideogratius@gmail.com>
2023-02-22 13:18:41 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,27 +1,42 @@
 #include <stdio.h>
 
 /**
- * main - print first 50 Fibonacci numbers
- * Return: 0
+ * print_fib_x50 - prints the first 50 Fibonacci numbers,
+ * starting with 1 and 2, followed by a new line.
+ *
+ * Return: void
  */
+void print_fib_x50(void)
+{
+	unsigned int target;
+	unsigned long int f, l, i, fib;
 
+	f = 1;
+	l = 2;
+	target = 50 - 2;
+	printf("1, 2, ");
+	for (i = 0 ; i < target ; i++)
+	{
+		fib = f + l;
+		printf("%lu", fib);
+		f = l;
+		l = fib;
+		if (i < target - 1)
+			printf(", ");
+	}
+	putchar('\n');
+}
+
+
+/**
+ * main - Entry point,
+ *
+ *
+ * Return: Always 0 (sucess)
+ */
 int main(void)
 {
-	int counter;
-	int countto = 50;
-	long a = 1;
-	long b = 2;
-
-	for (counter = 1; counter <= (countto / 2); counter++)
-	{
-		printf("%li %li ", a, b);
-		a += b;
-		b += a;
-	}
-	if (countto % 2 == 1)
-		printf("%li", a);
-
-	printf("\n");
-
+	print_fib_x50();
 	return (0);
 }
+
--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,46 +1,45 @@
 #include <stdio.h>
+/**
+ * print_fib - prints the first 50 Fibonacci numbers,
+ * starting with 1 and 2, followed by a new line.
+ * @len: the length of with the fib sequance should print to.
+ *
+ * Return: void
+ */
+void print_fib(int len)
+{
+	unsigned int target, i;
 
-/* program that finds and prints the first 98 Fibonacci numbers, starting with 1 and 2, followed by a new line */
+	unsigned int former_l, former_r,;
+	unsigned int latter_l, latter_r;
+	unsigned int fib_l, fib_r;
 
+	f = 1;
+	l = 2;
+	target = len - 2;
+	for (i = 0 ; i < target ; i++)
+	{
+		fib = f + l;
+		printf("%d : ", i + 4);
+		printf("%.0Lf" , fib);
+		f = l;
+		l = fib;
+		if (i < target - 1)
+			printf("\n");
+	}
+	putchar('\n');
+}
+
+
+/**
+ * main - Entry point,
+ *
+ *
+ * Return: Always 0 (sucess)
+ */
 int main(void)
 {
-	int counter, overflow;
-	unsigned long a = 1;
-	unsigned long b = 1;
-	unsigned long sum = 0;
-	long a_head, a_tail, b_head, b_tail, sum_head, sum_tail;
-
-	printf("1");
-
-	for (counter = 2; counter < 93; counter++) /* will still print */
-	{
-		sum = a + b;
-		a = b;
-		b = sum;
-		printf(", %lu", sum);
-	}
-
-	a_head = a / 1000000000; /* break larger num into 2 parts */
-	a_tail = a % 1000000000;
-	b_head = b / 1000000000;
-	b_tail = b % 1000000000;
-
-	for (; counter < 99; counter++)
-	{
-		overflow = (a_tail + b_tail) / 1000000000;
-		sum_tail = (a_tail + b_tail) - (1000000000 * overflow);
-		sum_head = (a_head + b_head) + overflow;
-
-		printf(", %lu%lu", sum_head, sum_tail);
-
-		a_head = b_head;
-		a_tail = b_tail;
-		b_head = sum_head;
-		b_tail = sum_tail;
-	}
-
-
-	printf("\n");
-
+	print_fib(98);
 	return (0);
 }
+
