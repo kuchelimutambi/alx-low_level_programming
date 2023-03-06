33e42fcf9b2067f473d3406c4a6c978e195f1979
kucheli mutambi <kuchelideogratius@gmail.com>
2023-03-06 20:58:53 +0300

    Create 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * _strspn - return length of string that matches values consistently
+ * @s: string to search
+ * @accept: target matches
+ * Return: number of bytes consecutively matched
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				break;
+			}
+		}
+		if (!accept[j])
+		{
+			break;
+		}
+
+	}
+	return (i);
+}
