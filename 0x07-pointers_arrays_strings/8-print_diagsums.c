d38f696fe6318d4ef7261ccde535779090ad8fc8
kucheli mutambi <kuchelideogratius@gmail.com>
2023-03-06 21:32:18 +0300

    Create 8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_chessboard - print chessboard given set 2D array
+ * @a: 2D array
+ */
+
+void print_chessboard(char (*a)[8])
+{
+  int i;
+	int j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+			if (j == 7)
+			{
+				_putchar('\n');
+			}
+		}
+	}
+}
