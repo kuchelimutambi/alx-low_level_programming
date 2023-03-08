f31a6e3c6a47e85cad70e505d62f0680c18827b5
kucheli mutambi <kuchelideogratius@gmail.com>
2023-03-08 16:10:10 +0300

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,13 +9,11 @@
 
 int find_root(int n, int root)
 {
-  if (x * x == n)
-	{
-		return (x);
-	}
-	else if (x * x > n)
-	{
+ if (root * root > n)
 		return (-1);
-	}
-	return (_sqrt_recursion_aux(n, x + 1));
+
+	if (root * root == n)
+		return (root);
+
+	return (find_root(n, root + 1));
 }
