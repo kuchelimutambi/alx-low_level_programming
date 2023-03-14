a343fbd48045051166d32d23395651467f6d6404
kucheli mutambi <kuchelideogratius@gmail.com>
2023-03-14 09:48:22 +0300

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,113 +1,63 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * _strlen - returns the length of a string
- *
- * @s: string type char
- * Return: returns the lenght of a string
+ * @s: string
+ * Return: length
  */
+
 int _strlen(char *s)
 {
-	int len;
+	int len = 0;
 
-	len = 0;
-	while (*s++ != '\0')
-		len++;
+	while (*s != '\0')
+		len++, s++;
 
 	return (len);
 }
 
 /**
- * count_words - counts the number of words in a string
- *
- * @str: The string
- * Return: The number of words or
- * any characters separated by spaces
+ * argstostr - concatenates all the arguments of your program
+ * @ac: argc
+ * @av: arguments
+ * Return: pointer to array
  */
-int count_words(char *str)
-{
-	int i;
-	int count = 0, word = 0;
 
-	i = 0;
-	while (str[i] != '\0')
+char *argstostr(int ac, char **av)
+{
+	char *s;
+	int len = 0, i, j, k = 0;
+
+	if (ac == 0 || av == NULL) /* validate input */
+		return (NULL);
+
+	/* find length to malloc */
+	for (i = 0; i < ac; i++)
 	{
-		if (str[i] != ' ' && word == 0)
-		{
-			count++;
-			word = 1;
-		}
-		else if (str[i] == ' ')
-		{
-			word = 0;
-		}
-		i++;
+		len += _strlen(av[i]);
+	}
+	len += (ac + 1); /* add space for newlines and null terminator */
+
+	/* allocate memory and free if error */
+	s = malloc(len * sizeof(char));
+
+	if (s == NULL)
+	{
+		free(s);
+		return (NULL);
 	}
 
-	return (count);
-}
-
-/**
- * free_gridc - frees a 2 dimensional grid
- * @grid: grid to free
- * @height: height of grid
- * Return: void
- */
-void free_gridc(char **grid, int height)
-{
-	int i;
-
-	i = 0;
-	while (i < height)
-		free(grid[i++]);
-
-	free(grid);
-}
-
-
-/**
- * strtow - function that splits a string into words.
- * @str: input
- * Return: a pointer to an array of strings (words)
- */
-char **strtow(char *str)
-{
-	int i, j = 0, k, temp_len = 0;
-	char **arr;
-
-	if (str == NULL || _strlen(str) == 0)
-		return (NULL);
-	if (count_words(str) == 0)
-		return (NULL);
-	arr = (char **)malloc((count_words(str) + 1) * sizeof(char *));
-	if (arr == NULL)
-		return (NULL);
-
-	for (i = 0; i < _strlen(str); i++)
+	/* insert each arg into *str */
+	for (i = 0; i < ac; i++)
 	{
-		if (str[i] != ' ')
+		for (j = 0; j < _strlen(av[i]); j++)
 		{
-			for (k = i, temp_len = 0; str[k] != '\0' && str[k] != ' '; k++)
-				temp_len++;
-
-			arr[j] = (char *)malloc((temp_len + 1) * sizeof(char));
-			if (arr[j] == NULL)
-			{
-				free_gridc(arr, k);
-				return (NULL);
-			}
-			k = 0;
-			while (str[i] != '\0' && str[i] != ' ')
-			{
-				arr[j][k] = str[i];
-				i++;
-				k++;
-			}
-			arr[j][k] = '\0';
-			j++;
+			s[k++] = av[i][j];
 		}
+		s[k++] = '\n';
 	}
-	arr[j] = NULL;
-	return (arr);
+
+	return (s);
 }
