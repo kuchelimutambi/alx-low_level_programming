5eb5977160cd4dec67d3946bc8836d888f19e943
kucheli mutambi <kuchelideogratius@gmail.com>
2023-03-22 20:15:09 +0300

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,54 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
 /**
- * op_add - add
- * @a: integer
- * @b: integer
- * Return: result
+ * op_add - function that returns the sum of a and b.
+ * @a: input
+ * @b: input
+ * Return: output
  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
-
 /**
- * op_sub - subtract
- * @a: integer
- * @b: integer
- * Return: result
+ * op_sub - function that returns the difference of a and b.
+ * @a: input
+ * @b: input
+ * Return: output
  */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
-
 /**
- * op_mul - multiply
- * @a: integer
- * @b: integer
- * Return: result
+ * op_mul - function that returns the product of a and b.
+ * @a: input
+ * @b: input
+ * Return: output
  */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
-
 /**
- * op_div - divide
- * @a: integer
- * @b: integer
- * Return: result
+ * op_div - function that returns the result of the division of a and b.
+ * @a: input
+ * @b: input
+ * Return: output
  */
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a / b);
 }
-
 /**
- * op_mod - modulus for remainder
- * @a: integer
- * @b: integer
- * Return: result
+ * op_mod - function that returns the remainder of the division of a and b.
+ * @a: input
+ * @b: input
+ * Return: output
  */
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
