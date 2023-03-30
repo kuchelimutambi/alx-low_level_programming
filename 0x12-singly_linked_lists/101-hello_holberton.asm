ea31f771841e8a8eec8eec0ba064c94b1274dcba
kucheli mutambi <kuchelideogratius@gmail.com>
2023-03-30 20:56:59 +0300

    Update 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,9 @@
-section .data
-    hello db 'Hello, Holberton', 0Ah ; the string to be printed
-
-section .text
-    global main
-    extern printf
-
+ global    main
+          extern    printf
 main:
-    push rbp
-    mov rbp, rsp
-
-    lea rdi, [hello] ; load the address of the string into rdi
-    xor eax, eax     ; zero out eax
-    call printf      ; call printf with the format string in rdi
-
-    mov rsp, rbp
-    pop rbp
-    ret
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
