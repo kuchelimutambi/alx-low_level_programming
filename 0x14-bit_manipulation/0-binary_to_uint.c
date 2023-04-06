71ba1092235caaa66174d8c514f5e8a617c56820
kucheli mutambi <kuchelideogratius@gmail.com>
2023-04-06 17:29:26 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,57 +7,33 @@
  */
  
 unsigned int binary_to_uint(const char *b)
+
 {
+
 	int len = 0, i;
 	unsigned int sum = 0;
 
 	if (b == NULL)
-    	{
-        return (0);
-    	}
+		return (0);
 
-    	unsigned int result = 0;
-    	size_t len = strlen(b);
-    	int i;
-
-    	for (i = 0; i < len; i++)
-    	{
-        if (isdigit(b[i]))
-        {
-        result = result << 1;
-        if (b[i] == '1')
-        {
-        result |= 1;
-        }
-        }
-        else
-        {
-        return (0);
-        }
-    	}	
-
-    	return (result);
-
-	return (sum);
-
-	/* Find string length */
+	/* find string length */
 	while (b[len] != '\0')
-	len++;
+		len++;
 	len -= 1;
 
-	/* Iterate string and multiply by power of 2 if character is '1' */
+	/* iterate string and if '1' then multiply by power of 2 */
+	/* get power of 2 via binary (e.g. 1<<2 = 100 in binary = 4) */
 	i = 0;
 	while (b[i])
 	{
-	if ((b[i] != '0') && (b[i] != '1'))
-	return (sum);
+		if ((b[i] != '0') && (b[i] != '1'))
+			return (sum);
 
-	if (b[i] == '1')
-	sum += (1 * (1 << len));
-	i++;
-	len--;
+		if (b[i] == '1')
+			sum += (1 * (1 << len));
+		i++;
+		len--;
 	}
 
 	return (sum);
-
 }
