fd3d0d6cc1195ebd07ed5c3a8f8a678e5370a382
kucheli mutambi <kuchelideogratius@gmail.com>
2023-04-10 19:05:52 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,27 +1,38 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
 
-int create_file(const char *filename, char *text_content) {
-    if (filename == NULL) {
-        return -1;
-    }
+/**
+ * create_file - creates file with permissions rw------- and writes content in
+ * if file already exists, don't change permissions and just truncate it
+ * @filename: name to give to new file
+ * @text_content: writes this content into file
+ * Return: 1 on success, -1 on error
+ */
+int create_file(const char *filename, char *text_content)
+{
+    int fd, n_wrote, len = 0;
 
-    int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-    if (fd == -1) {
-        return -1;
-    }
+    if (filename == NULL)
+        return (-1);
 
-    if (text_content != NULL) {
-        int bytes_written = write(fd, text_content, strlen(text_content));
-        if (bytes_written == -1) {
+    /* open file for writing, truncating it if it exists */
+    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    if (fd == -1)
+        return (-1);
+
+    if (text_content != NULL)
+        len = _strlen(text_content);
+
+    /* write content if provided */
+    if (len > 0)
+    {
+        n_wrote = write(fd, text_content, len);
+        if (n_wrote == -1 || n_wrote != len)
+        {
             close(fd);
-            return -1;
+            return (-1);
         }
     }
 
     close(fd);
-    return 1;
+    return (1);
 }
