5c5606d8bc5e9e29e249b0ab032aedff29506f01
kucheli mutambi <kuchelideogratius@gmail.com>
2023-04-10 19:30:06 +0300

    Update 100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,66 +1,53 @@
-#include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <elf.h>
-
-#define ERR_EXIT(msg) do { fprintf(stderr, "%s\n", msg); exit(98); } while (0)
-
-int main(int argc, char *argv[])
+/**
+ * main - Displays the information contained in the
+ * ELF header at the start of an ELF file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 on success.
+ *
+ * Description: If the file is not an ELF File or
+ * the function fails - exit code 98.
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-    if (argc != 2) {
-        ERR_EXIT("Usage: elf_header elf_filename");
-    }
+	Elf64_Ehdr *header;
+	int o, r;
 
-    int fd = open(argv[1], O_RDONLY);
-    if (fd == -1) {
-        ERR_EXIT("Failed to open file");
-    }
+	o = open(argv[1], O_RDONLY);
+	if (o == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+		exit(98);
+	}
+	header = malloc(sizeof(Elf64_Ehdr));
+	if (header == NULL)
+	{
+		close_elf(o);
+		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+		exit(98);
+	}
+	r = read(o, header, sizeof(Elf64_Ehdr));
+	if (r == -1)
+	{
+		free(header);
+		close_elf(o);
+		dprintf(STDERR_FILENO, "Error: `%s`: No such file\n", argv[1]);
+		exit(98);
+	}
 
-    // Read and check ELF magic number
-    char magic[EI_NIDENT];
-    if (read(fd, magic, EI_NIDENT) != EI_NIDENT ||
-        magic[EI_MAG0] != ELFMAG0 || magic[EI_MAG1] != ELFMAG1 ||
-        magic[EI_MAG2] != ELFMAG2 || magic[EI_MAG3] != ELFMAG3) {
-        ERR_EXIT("Not an ELF file");
-    }
+	check_elf(header->e_ident);
+	printf("ELF Header:\n");
+	print_magic(header->e_ident);
+	print_class(header->e_ident);
+	print_data(header->e_ident);
+	print_version(header->e_ident);
+	print_osabi(header->e_ident);
+	print_abi(header->e_ident);
+	print_type(header->e_type, header->e_ident);
+	print_entry(header->e_entry, header->e_ident);
 
-    // Read and check ELF class (32-bit or 64-bit)
-    int elf_class = magic[EI_CLASS];
-    if (elf_class != ELFCLASS32 && elf_class != ELFCLASS64) {
-        ERR_EXIT("Invalid ELF class");
-    }
-
-    // Set file offset to start of ELF header
-    if (lseek(fd, 0, SEEK_SET) != 0) {
-        ERR_EXIT("Failed to set file offset");
-    }
-
-    // Read and interpret ELF header
-    Elf32_Ehdr ehdr32;
-    Elf64_Ehdr ehdr64;
-    if (elf_class == ELFCLASS32) {
-        if (read(fd, &ehdr32, sizeof(ehdr32)) != sizeof(ehdr32)) {
-            ERR_EXIT("Failed to read ELF header");
-        }
-        printf("Magic:   %02x %02x %02x %02x\n", magic[EI_MAG0], magic[EI_MAG1], magic[EI_MAG2], magic[EI_MAG3]);
-        printf("Class:   ELF32\n");
-        printf("Data:    %s\n", magic[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" :
-                                 magic[EI_DATA] == ELFDATA2MSB ? "2's complement, big endian" :
-                                 "Invalid data encoding");
-        printf("Version: %d\n", magic[EI_VERSION]);
-        printf("OS/ABI:  %d\n", ehdr32.e_ident[EI_OSABI]);
-        printf("ABI Version: %d\n", ehdr32.e_ident[EI_ABIVERSION]);
-        printf("Type:    %d\n", ehdr32.e_type);
-        printf("Entry point address: %#x\n", ehdr32.e_entry);
-    } else {
-        if (read(fd, &ehdr64, sizeof(ehdr64)) != sizeof(ehdr64)) {
-            ERR_EXIT("Failed to read ELF header");
-        }
-        printf("Magic:   %02x %02x %02x %02x\n", magic[EI_MAG0], magic[EI_MAG1], magic[EI_MAG2], magic[EI_MAG3]);
-        printf("Class:   ELF64\n");
-        printf("Data:    %s\n", magic[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" :
-                                 magic[EI_DATA] == ELFDATA2MSB ? "2's complement, big endian" :
-                                 "Invalid data encoding");
-        printf("Version: %d\n", magic[EI
+	free(header);
+	close_elf(o);
+	return (0);
+}
